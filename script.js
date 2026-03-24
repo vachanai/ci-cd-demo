e29a56c34abf1256c8010df30377ef20cdb1388c
mick <manonai.v@gmail.com>
2026-03-24 16:12:07 +0700

    Update test local server

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ const changeTextBtn = document.getElementById("changeTextBtn");
 const message = document.getElementById("message");
 
 changeTextBtn.addEventListener("click", () => {
-    message.textContent = "ข้อความนี้มาจาก JavaScript";
+    message.textContent = "JavaScript ทำงานแล้ว และไฟล์นี้มาจากโฟลเดอร์ local";
 });
